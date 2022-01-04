78c04458d26dfe30872ba1f7a2a24f14ec80ef0a
TrinidadCarlos <carlostrinidad952@gmail.com>
2022-01-04 14:15:01 -0600

    se hacen modificaciones para deploy en vercel

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 //firebase
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -16,7 +16,8 @@ const firebaseConfig = {
 };
 
 //firebase
-const app = initializeApp(firebaseConfig); // Initialize Firebase
+// const app = initializeApp(firebaseConfig); // Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const auth = getAuth(); //authentication
 const db = getFirestore(); // cloud firestore (database)
 const storage = getStorage();
